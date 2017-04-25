bcb47510dcfec82b8360cc9a8ea61493c8ca19ce
Gyorgy Orosz <oroszgy@gmail.com>
2017-04-25 17:47:59 +0200

    Improved performance on word counting.

--- training/plain_word_freqs.py
+++ training/plain_word_freqs.py
@@ -2,31 +2,46 @@ from __future__ import unicode_literals
 
 import codecs
 import glob
+import json
+import os
+import shutil
+import tempfile
 from collections import Counter
+from multiprocessing import Pool
 
 import plac
-from multiprocessing import Pool
+import six
 from tqdm import tqdm
+
+TMP_DIR = tempfile.mkdtemp()
+print(TMP_DIR)
+print("A")
 
 
 def count_words(fpath):
+    fname = os.path.split(fpath)[-1]
+    outfpath = os.path.join(TMP_DIR, fname)
     with codecs.open(fpath, encoding="utf8") as f:
         words = f.read().split()
-        counter = Counter(words)
-    return counter
+        counter = dict(Counter(words))
+        json.dump(counter, open(outfpath, "w"))
+    return outfpath
 
 
 def main(input_glob, out_loc, workers=4):
     p = Pool(processes=workers)
-    counts = p.map(count_words, tqdm(list(glob.glob(input_glob))))
+    freq_path = p.map(count_words, glob.iglob(input_glob))
     df_counts = Counter()
     word_counts = Counter()
-    for wc in tqdm(counts):
-        df_counts.update(wc.keys())
-        word_counts.update(wc)
+    for fp in tqdm(freq_path):
+        with open(fp) as f:
+            wc = json.load(f)
+            df_counts.update(wc.keys())
+            word_counts.update(wc)
     with codecs.open(out_loc, "w", encoding="utf8") as f:
-        for word, df in df_counts.iteritems():
+        for word, df in six.iteritems(df_counts):
             f.write(u"{freq}\t{df}\t{word}\n".format(word=repr(word), df=df, freq=word_counts[word]))
+    shutil.rmtree(TMP_DIR)
 
 
 if __name__ == "__main__":
